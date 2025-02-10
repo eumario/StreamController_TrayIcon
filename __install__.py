60ec3cd1c47b08526f8c6923c90b13ab40aed5a8
Mario Steele <mario@ruby-im.net>
2025-02-09 21:40:27 -0600

    Testing to see if we can use base packages.

--- __install__.py
+++ __install__.py
@@ -1,5 +1,14 @@
-from streamcontroller_plugin_tools.installation_helpers import create_venv
+from venv import create
 from os.path import join, abspath, dirname
+from subprocess import run
+
+def create_venv(path: str = ".venv", path_to_requirements_txt: str = None) -> None:
+    create(path, system_site_packages=True, with_pip=True)
+
+    if path_to_requirements_txt is None:
+        return
+    print(f". {join(path, 'bin', 'activate')} && pip install -r {path_to_requirements_txt}")
+    run(f". {join(path, 'bin', 'activate')} && pip install -r {path_to_requirements_txt}", start_new_session=True, shell=True)
 
 toplevel = dirname(abspath(__file__))
 create_venv(join(toplevel, "backend", ".venv"), join(toplevel, "backend", "requirements.txt"))
